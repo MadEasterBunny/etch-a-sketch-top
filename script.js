f341a190a62fad5499dace11ae663e8333735be8
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2025-11-08 08:45:45 +0900

    Fix grid JS and CSS

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector("#container");
 
-for(let i = 0; i < 16; i++) {
+let gridSize = 16;
+
+for(let i = 0; i < (gridSize * gridSize); i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
@@ -9,6 +11,6 @@ for(let i = 0; i < 16; i++) {
 const squares = container.querySelectorAll(".square");
 squares.forEach(square => {
     square.addEventListener("mouseover", () => {
-        square.style.opacity = 1;
+        square.style.backgroundColor = "#000";
     })
 })
