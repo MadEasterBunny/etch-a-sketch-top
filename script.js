9407707d440a60df4b3d1dc75d9d9b5a31870769
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2025-11-08 11:30:27 +0900

    Add color changing buttons and functionality

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const container = document.querySelector("#container");
 const resizeGridBtn = document.querySelector("#resize-grid-btn");
+const randomColorsBtn = document.querySelector("#random-colors");
+const blackBtn = document.querySelector("#black");
 
 let gridSize = 16;
+let blackColor = true;
+
+const randomNum = (num) => {
+    return Math.floor(Math.random() * num);
+}
 
 const generateGrid = () => {
     container.replaceChildren();
@@ -18,7 +25,11 @@ const generateGrid = () => {
 const colorSquares = (squares) => {
     squares.forEach(square => {
     square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "#000";
+        if(blackColor) {
+            square.style.backgroundColor = "#000";
+        } else {
+            square.style.backgroundColor = `rgb(${randomNum(255)} ${randomNum(255)} ${randomNum(255)})`;
+        }
     })
 })
 }
@@ -40,4 +51,12 @@ resizeGridBtn.addEventListener("click", () => {
         const squares = container.querySelectorAll(".square");
         colorSquares(squares);
     }
+})
+
+blackBtn.addEventListener("click", () => {
+    blackColor = true;
+})
+
+randomColorsBtn.addEventListener("click", () => {
+    blackColor = false;
 })
