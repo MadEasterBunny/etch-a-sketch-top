d6471457183c0f72055576396ea861cab49aea37
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2025-11-08 11:18:48 +0900

    Add grid resize button and functionality

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const container = document.querySelector("#container");
+const resizeGridBtn = document.querySelector("#resize-grid-btn");
 
 let gridSize = 16;
 
-for(let i = 0; i < (gridSize * gridSize); i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+const generateGrid = () => {
+    container.replaceChildren();
+
+    for(let i = 0; i < (gridSize * gridSize); i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+
+    document.documentElement.style.setProperty("--size", gridSize);
 }
 
-const squares = container.querySelectorAll(".square");
-squares.forEach(square => {
+const colorSquares = (squares) => {
+    squares.forEach(square => {
     square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "#000";
     })
+})
+}
+
+generateGrid();
+
+const squares = container.querySelectorAll(".square");
+colorSquares(squares);
+
+resizeGridBtn.addEventListener("click", () => {
+    const input = prompt("Enter grid size", "");
+    gridSize = parseInt(input);
+
+    if(input > 100) {
+        alert("Grid size cannot be larger than 100 x 100");
+    } else {
+        generateGrid();
+
+        const squares = container.querySelectorAll(".square");
+        colorSquares(squares);
+    }
 })
